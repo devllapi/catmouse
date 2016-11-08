8f85bb60f6f6301c4cd06260440b534bc4bf7b13
Michael Dellapi <mad758@nyu.edu>
2016-11-08 00:17:26 -0500

    First commit, mouse script implemented

---
+++ Assets/Scripts/mouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouse : MonoBehaviour {
+	public Transform cat;
+	Vector3 directionToCat;
+	public Rigidbody rb;
+	public float thrust=20f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		directionToCat = cat.position - transform.position;
+		if (Vector3.Angle (transform.forward, directionToCat) < 180f) {
+			Ray mouseRay = new Ray (transform.position, directionToCat);
+			RaycastHit mouseRayHitInfo;
+			if(Physics.Raycast(mouseRay, out mouseRayHitInfo, 50f)){
+				Debug.DrawRay (mouseRay.origin, mouseRay.direction * 100f, Color.red);
+				if(mouseRayHitInfo.collider.tag=="Cat"){
+					rb.AddForce (-directionToCat.normalized * 1000f);
+					Debug.Log ("fear");
+				}
+				
+		}
+	}
+}
+					}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	float velocity;
+	float turn;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		 rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		turn = Random.value;
+		//RaycastHit hit;
+		rb.velocity = transform.forward * 10f + Physics.gravity;
+		Ray moveRay = new Ray (transform.position, transform.forward);
+		if(Physics.SphereCast(moveRay,1f, 1f)){
+			Debug.DrawRay(moveRay.origin, moveRay.direction*1000f, Color.yellow);
+			if (turn > .5f) {
+				transform.Rotate (0f, 90f, 0f);	
+			}else if(turn<.5f){
+				transform.Rotate (0f, -90f, 0f);
+	}
+			}}
+}
